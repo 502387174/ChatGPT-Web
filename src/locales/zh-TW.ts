2ecd09a30884bcdb2d40da830b406242e5a98474
Love to drink coffee <502387174@qq.com>
2023-03-17 17:58:58 +0800

    Update zh-TW.ts

--- src/locales/zh-TW.ts
+++ src/locales/zh-TW.ts
@@ -13,7 +13,7 @@ export default {
     success: '操作成功',
     failed: '操作失敗',
     verify: '驗證',
-    unauthorizedTips: '人机验证，扫码回复：验证码',
+    unauthorizedTips: '人机验证，扫码回复：gpt',
   },
   chat: {
     placeholder: '來說點什麼...（Shift + Enter = 換行）',
