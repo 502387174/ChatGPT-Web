94d9017571abd3f3c7af60b96ba95091f9d257ab
Love to drink coffee <502387174@qq.com>
2023-03-17 17:57:38 +0800

    Update Permission.vue

--- src/views/chat/layout/Permission.vue
+++ src/views/chat/layout/Permission.vue
@@ -56,9 +56,7 @@ function handlePress(event: KeyboardEvent) {
     <div class="p-10 bg-white rounded dark:bg-slate-800">
       <div class="space-y-4">
         <header class="space-y-2">
-          <h2 class="text-2xl font-bold text-center text-slate-800 dark:text-neutral-200">
-            403
-          </h2>
+					<image src="http://niubility.cloud/profile/blog/2023/03/8b3c7352d972bf69_20230316141826A001.jpg" width="200px"  />
           <p class="text-base text-center text-slate-500 dark:text-slate-500">
             {{ $t('common.unauthorizedTips') }}
           </p>
